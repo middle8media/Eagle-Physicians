90609973cbf79939910d7e5b5bd1fb052d477a08
middle8media <seth@middle8media.com>
2013-02-28 12:35:24 -0500

    Final Commit for Mike

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 $(function() {
     $('.folder-abt, .folder-drs, .folder-loc, .folder-nws, .folder-dia, .folder-log, .folder-egl').hover(function() {
         var index = $(this).index();
-        var elem = $('h3').get(index);
+        var elem = $('span').get(index);
         $(elem).addClass('h3-hover');
     }, function() {
         $('.h3-hover').removeClass('h3-hover');
--- js/lte-ie8.js
+++ js/lte-ie8.js
@@ -4,38 +4,9 @@
 $(function() {
     $('.folder-abt, .folder-drs, .folder-loc, .folder-nws, .folder-dia, .folder-log, .folder-egl').hover(function() {
         var index = $(this).index();
-        var elem = $('h3').get(index);
+        var elem = $('span').get(index);
         $(elem).addClass('h3-hover');
     }, function() {
         $('.h3-hover').removeClass('h3-hover');
     });
 });
-
-
-//folder zoom to URL animation
-
-// $(document).ready(function() {
-// $('body').fadeIn(1000);
-
-//   $('.folder-group a').click(function(event) {
-//     event.preventDefault(event);
-//     newLocation = this.href;
-//     that = $(this);
-
-//       $(this).parent().animate({left: '300px'}, 200)
-
-//       setTimeout(function() {
-//         that.parent().addClass('zoom');
-//         $('.left-folder').animate({opacity: 0}, 1000)
-//       }, 1000);
-
-//       setTimeout(function() {
-//         $('body').fadeOut(500, newpage);
-//       }, 2000);
-
-//       function newpage() {
-//       window.location = newLocation;
-//       };
-
-//   });
-// });
